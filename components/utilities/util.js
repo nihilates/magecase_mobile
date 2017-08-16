1d47b148c99a6d67ab8297b0b649e70e4a7d3e7d
nihilates <nihilates@gmail.com>
2017-08-15 20:45:45 -0400

    Moved the app utilities file to the main component directory

--- components/utilities/util.js
+++
@@ -1,6 +0,0 @@
-/*Helper Functions for use components*/
-module.exports = { //returns one or the other component based on a condition check
-  binaryRender: (condition, main, other) => { //takes a condition as the 1st parameter and two components as the 2nd and 3rd.
-    return condition ? main : other;
-  }
-};
