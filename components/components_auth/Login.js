0820a652acd33a1cc5e22eefdd54ec3071610a21
nihilates <nihilates@gmail.com>
2017-08-31 21:37:17 -0400

    Code cleanup for Login component

--- components/components_auth/Login.js
+++ components/components_auth/Login.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
 import {
   Alert,
-  AsyncStorage,
   TextInput,
-  TouchableOpacity,
   StyleSheet,
   Text,
   View
 } from 'react-native';
 import { Actions } from 'react-native-router-flux';
-import { SimpleBtn } from '../components_misc/BasicCmpnts.js';
 import { saveToken } from '../_util.js';
 import axios from 'axios'; //axios for AJAX calls
 //import api configurations
 import { path, api } from '../_config.js';
+//import custom components
+import { SimpleBtn } from '../components_misc/BasicCmpnts.js';
 
-class Login extends Component {
+export default class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {username: '', password: ''};
   }
 
-  userLogin() {
-    if (!this.state.username || !this.state.password) return;
+  userLogin() { //Method to submit login credentials
+    if (!this.state.username || !this.state.password) return; //if there is no username OR password, return empty (do not proceed)
     axios.get(path+api.user.login, {
       params: {
         identity: this.state.username,
@@ -30,13 +29,13 @@ class Login extends Component {
       }
     })
     .then(res => {
-      if (res.status === 200) {
-        saveToken('session', res.data);
-        Actions.Home();
-      } else if (res.status === 204) {
-        Alert.alert('Incorrect username or password', 'Please try again.');
+      if (res.status === 200) { //if the response was 200...
+        saveToken('session', res.data); //save the returned data (non-sensitive account info & JSON webtoken)
+        Actions.Home(); //transition page to the "Home" component
+      } else if (res.status === 204) { //if the response was 204...
+        Alert.alert('Incorrect username or password', 'Please try again.'); //send alert that either a username or password was incorrect
       } else {
-        Alert.alert('Something went wrong', 'Please restart the app.')
+        Alert.alert('Something went wrong', 'Please try again later.'); //if this error is thrown, something must be wrong on the server's side
       }
     })
     .catch(error => console.error(error));
@@ -48,7 +47,7 @@ class Login extends Component {
         <Text style={s.title}>Sign In</Text>
 
         <View>
-          <TextInput
+          <TextInput //Text field to enter a username OR email address (both are accepted by the server)
             editable={true}
             autoCorrect={false}
             autoCapitalize={'none'}
@@ -59,7 +58,7 @@ class Login extends Component {
             style={s.input}
             value={this.state.username}
           />
-          <TextInput
+          <TextInput //Text field for password
             editable={true}
             autoCorrect={false}
             onChangeText={(password) => this.setState({password})}
@@ -73,13 +72,14 @@ class Login extends Component {
           />
         </View>
 
-        <SimpleBtn callback={this.userLogin.bind(this)} buttonText="Login"/>
+        <SimpleBtn
+          callback={this.userLogin.bind(this)}
+          buttonText="Login"
+        />
       </View>
     )
   }
 }
-
-export default Login;
 
 const s = StyleSheet.create({
   container: {
